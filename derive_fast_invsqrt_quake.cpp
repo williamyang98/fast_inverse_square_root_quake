6530e295cacbca3ca99375a0921172e2d89b9ba1
William Yang <williamyang2017@gmail.com>
2024-06-08 21:31:43 +1000

    Added p-norm loss function to Quake solver

--- derive_fast_invsqrt_quake.cpp
+++ derive_fast_invsqrt_quake.cpp
@@ -13,93 +13,102 @@ static inline int32_t quantise_float(float x) {
     return y.i32;
 }
 
+template <size_t N, typename T>
+static inline T get_nth_power(T x) {
+    if constexpr(N == 0) return T(1);
+    if constexpr(N == 1) return x;
+    T y = get_nth_power<N/2, T>(x);
+    T y_2 = y*y;
+    if constexpr(N % 2 == 0) return y_2;
+    return x*y_2;
+}
+
 // clang++ main.cpp -o main -std=c++17 -O3 -march=native
 int main(int argc, char** argv) {
     // Qy ~ 2^23*(190.5 - 1.5*k0') - 0.5*Qx
-    // 2*Qy ~ 2^23*381 - 2^23*3*k0' - Qx
-    // Jy ~ 2^23*381 - 2^23*3*k0' - Qx
-    // Jy ~ 2^23*381 - k1 - Qx
-    constexpr int64_t M0 = int64_t(381) << 23;
+    // Qy ~ 2^22*381 - 2^22*3*k0' - 0.5*Qx
+    // Qy ~ 2^22*381 - k1 - 0.5*Qx
+    constexpr int64_t M0 = int64_t(381) << 22;
 
     std::vector<int64_t> QX;
-    std::vector<int64_t> JY_target;
+    std::vector<int64_t> QY_target;
     const auto push_sample = [&](float x) {
         const float y = 1.0f/std::sqrt(x);
         const int32_t Qx = quantise_float(x);
         const int32_t Qy = quantise_float(y);
-        const int64_t Jy = int64_t(Qy)*2;
         QX.push_back(int64_t(Qx));
-        JY_target.push_back(Jy);
+        QY_target.push_back(int64_t(Qy));
     };
 
-    for (float x = 1e-7f; x <= 1e-6f; x += 1e-9f) push_sample(x);
-    for (float x = 1e-6f; x <= 1e-5f; x += 1e-8f) push_sample(x);
-    for (float x = 1e-5f; x <= 1e-4f; x += 1e-7f) push_sample(x);
-    for (float x = 1e-4f; x <= 1e-3f; x += 1e-6f) push_sample(x);
-    for (float x = 1e-3f; x <= 1e-2f; x += 1e-5f) push_sample(x);
-    for (float x = 1e-2f; x <= 1e-1f; x += 1e-4f) push_sample(x);
+    // Relative error is periodic so we only need to sample a single decade
     for (float x = 1e-1f; x <= 1e0f; x += 1e-3f) push_sample(x);
-    for (float x = 1e0f; x <= 1e1f; x += 1e-2f) push_sample(x);
-    for (float x = 1e1f; x <= 1e2f; x += 1e-1f) push_sample(x);
-    for (float x = 1e2f; x <= 1e3f; x += 1e0f) push_sample(x);
-    for (float x = 1e3f; x <= 1e4f; x += 1e1f) push_sample(x);
-    for (float x = 1e4f; x <= 1e5f; x += 1e2f) push_sample(x);
-    for (float x = 1e5f; x <= 1e6f; x += 1e3f) push_sample(x);
-    for (float x = 1e6f; x <= 1e7f; x += 1e4f) push_sample(x);
-    for (float x = 1e7f; x <= 1e8f; x += 1e5f) push_sample(x);
-
-    const size_t N = JY_target.size();
+    const size_t N = QX.size();
     printf("training over %zu samples\n", N);
+
+    // Increase P_norm to trade off lower mean squared error for lower max absolute error
+    // constexpr size_t P_norm = 6; // produces solution closer to original Quake
+    // constexpr double P_norm_scale = 4e4;
+    // constexpr double grad_scale = 1e6;
+    // constexpr size_t P_norm = 4;
+    // constexpr double P_norm_scale = 1e5;
+    // constexpr double grad_scale = 1e7;
+    constexpr size_t P_norm = 2; // mean squared error
+    constexpr double P_norm_scale = 1e8;
+    constexpr double grad_scale = 1e8;
 
     int64_t k1 = 0;
     int64_t best_k1 = k1;
-    uint64_t best_mse = ~uint64_t(0);
+    double best_loss = double(~uint64_t(0));
     size_t best_iter = 0;
-    constexpr size_t TOTAL_ITERATIONS = 1024;
+    constexpr size_t TOTAL_ITERATIONS = 102400;
     constexpr size_t PRINT_ITER = TOTAL_ITERATIONS / 32;
     for (size_t iter = 0; iter < TOTAL_ITERATIONS; iter++) {
-        int64_t mean_error = 0;
-        uint64_t mean_square_error = 0;
+        double mean_error = 0;
+        double mean_loss = 0;
         for (size_t i = 0; i < N; i++) {
-            // Jy ~ 2^23*381 - k1 - Qx
-            // error = 0.5*(Jy - Jy')^2
-            // error = 0.5*(2^23*381 - k1 - Qx - Qy')^2
-            // de/dk = -(Qy-Qy')
+            // Qy ~ 2^22*381 - k1 - 0.5*Qx
             const int64_t Qx = QX[i];
-            const int64_t Jy_target = JY_target[i];
-            const int64_t Jy_pred = M0 - k1 - Qx;
-            const int64_t error = Jy_pred - Jy_target;
-            mean_error += error;
-            mean_square_error += uint64_t(error*error);
+            const int64_t Qy_target = QY_target[i];
+            const int64_t Qy_pred = M0 - k1 - (Qx >> 1); // Qx > 0
+            // error = 1/n*(1 - Qy/Qy')^n
+            // de/dk = (1-Qy/Qy')^(n-1) * 1/Qy'
+            const double Qy_target_f64 = double(Qy_target);
+            const double error = (1.0 - double(Qy_pred)/Qy_target_f64)*P_norm_scale;
+            const double error_n = get_nth_power<P_norm-2>(error);
+            const double loss = get_nth_power<P_norm>(error);
+            mean_error += error*error_n/Qy_target_f64;
+            mean_loss += loss;
         }
-        mean_error /= int64_t(N);
-        mean_square_error /= int64_t(N);
+        mean_error /= double(N);
+        mean_loss /= double(N);
 
-        if (mean_square_error < best_mse) {
-            best_mse = mean_square_error;
+        if (mean_loss < best_loss) {
+            best_loss = mean_loss;
             best_k1 = k1;
             best_iter = iter;
         }
 
-        int64_t gradient = mean_error;
+        const int64_t gradient = -int64_t(mean_error*grad_scale);
         k1 += gradient;
-        if (iter % PRINT_ITER == 0 || mean_error == 0) {
-            printf("[%.3zu] mse=%" PRIu64 ", mean_error=%" PRIi64 "\n", iter, mean_square_error, mean_error);
+        if (iter % PRINT_ITER == 0 || gradient == 0) {
+            printf("iter=%.3zu, loss=%.3e, mean_error=%.3e, gradient=%" PRIi64 "\n", 
+                iter, mean_loss, mean_error, gradient);
         }
-        if (mean_error == 0) {
+        if (gradient == 0) {
             break;
         }
     }
 
     printf("\n[BEST RESULTS]\n");
+    printf("p-norm = %zu\n", P_norm);
     printf("iter = %zu\n", best_iter);
-    printf("mse = %" PRIu64 "\n", best_mse);
+    printf("loss = %.3e\n", best_loss);
     printf("k1 = %" PRIi64 "\n", best_k1);
-    // k1 = 2^23 * 3 * k0'
-    const double k0_ = double(best_k1)/double(int64_t(3) << 23);
+    // k1 = 2^22*3*k0'
+    const double k0_ = double(best_k1)/double(int64_t(3) << 22);
     printf("k' = %.8f\n", k0_);
-    // k0_ = (2^23*381 - k1) / 2
-    const int32_t k0 = int32_t((M0 - best_k1)/2);
+    // k0 = 2^22*381 - k1
+    const int32_t k0 = int32_t(M0 - best_k1);
     printf("k0 = %" PRIi32 "\n", k0);
     // Qy = k0 - 0.5*Qx
 
